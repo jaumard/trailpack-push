fcfd5ec1393160cdb6376097b2cb51d1a663f0e6
jaumard <ioquoo3H>
2016-02-07 18:52:36 +0100

    Fix eslint errors

--- api/services/PushService.js
+++ api/services/PushService.js
@@ -11,35 +11,33 @@ const _ = require('lodash')
  */
 module.exports = class PushService extends Service {
   sendToAPN(token, message) {
-    var options = this.app.config.push.apn;
+    const options = this.app.config.push.apn;
 
-    if (!Array.isArray(token))
-    {
+    if (!Array.isArray(token)) {
       token = [token];
     }
 
-    var apnConnection = new apn.Connection(options);
+    const apnConnection = new apn.Connection(options);
 
-    var note = new apn.Notification();
-    note     = _.merge(note, message);
+    let note = new apn.Notification();
+    note = _.merge(note, message);
 
-    for (var i = 0; i < token.length; i++)
-    {
-      var obj      = token[i];
-      var myDevice = new apn.Device(obj);
+    for (let i = 0; i < token.length; i++) {
+      const obj = token[i];
+      const myDevice = new apn.Device(obj);
       apnConnection.pushNotification(note, myDevice);
     }
   }
 
   sendToGCM(ids, messageInfos, retry, next) {
-    if(!Array.isArray(ids)) {
+    if (!Array.isArray(ids)) {
       ids = [ids]
     }
 
     ids = _.chunk(ids, 1000)
 
     let message
-    if(messageInfos) {
+    if (messageInfos) {
       message = new gcm.Message(messageInfos)
     }
     else {
@@ -47,12 +45,12 @@ module.exports = class PushService extends Service {
     }
 
     // Set up the sender with you API key
-    var sender = new gcm.Sender(this.app.config.push.gcm.senderId)
+    const sender = new gcm.Sender(this.app.config.push.gcm.senderId)
 
-    for(var i = 0; i < ids.length; i++) {
-      var obj = ids[i]
+    for (let i = 0; i < ids.length; i++) {
+      const obj = ids[i]
 
-      if(retry) {
+      if (retry) {
         sender.send(message, obj, next)
       }
       else {
